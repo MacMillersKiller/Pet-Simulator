cce6bde55c45f1cd779fe2a3666051e7ec0c311a
macmillerskiller <36bchambers@gmail.com>
2020-06-20 20:38:28 -0400

    first commit

---
+++ src/main/java/com/PetSimulator/PetSimulator/PetSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.PetSimulator.PetSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PetSimulator/PetSimulator/PetSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PetSimulator.PetSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
